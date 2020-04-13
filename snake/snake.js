e660d9ca6137023a9dba24ac34ecdabeaa4ff69b
ninjafat <davide.mezzetti@hotmail.it>
2020-04-14 01:49:38 +0200

    work in progress

--- snake/snake.js
+++ snake/snake.js
@@ -12,25 +12,68 @@ function shuffle(array) {
 
 const MAX_PIXEL = 20000;
 const MIN_PIXEL = 0;
+/*  keyboard control */
+const UP = 38;
+const LEFT = 37;
+const RIGHT = 39;
+const DOWN = 40;
+const PAUSE = 32;
+/** */
 let GAME_LOOP = false;
 let gameId 
+let snakeDirection = 'RIGHT'
+let fast = 200; //ms update snake position
+const snake = {
+    head: '',
+    tail: []
+}
 
+/**
+ * snake non può tornare indietro, su sè stesso
+ */
+window.addEventListener('keydown', function(e) {
+    switch (e.keyCode){
+        case LEFT:
+            if (snakeDirection !== RIGHT) {
+                snakeDirection = LEFT;
+            }
+            break 
+        case UP:
+            if (snakeDirection !== DOWN) {
+                snakeDirection = UP;
+            }
+            break
+        case RIGHT:
+            if (snakeDirection !== LEFT) {
+                snakeDirection = RIGHT;
+            }
+            break
+        case DOWN:
+            if (snakeDirection !== UP) {
+                snakeDirection = DOWN;
+            }
+            break
+        case '32':
+            /** TO DO PAUSE */
+            break
+        default:
+            snakeDirection = RIGHT;
+        
+    }
+})
 
 window.addEventListener('load', function(e) {
     console.log("document.onload", e)
-    let grid = document.getElementById('grid'); 
+    const grid = document.getElementById('grid'); 
     const score = document.getElementById('score');
     const buttonStart = document.getElementById('start');
-
-   let snakeDirection = 'RIGHT'
+    const eatingSoundEl = document.getElementById("audio");
 
     buttonStart.addEventListener('click', function(e) {
-        console.log("Clicked: ", e)
         GAME_LOOP = !GAME_LOOP
         if(!GAME_LOOP) clearInterval(gameId)
     })
 
-  
     function createBoard() {
         for(let i = 0; i < MAX_PIXEL; i++) {
             let div = document.createElement('DIV')
@@ -39,11 +82,14 @@ window.addEventListener('load', function(e) {
             grid.appendChild(div)
         }
     }
+
     
     function createSnake() {
         let position = getRandomNumber(MAX_PIXEL, MIN_PIXEL)
         let div = document.getElementById(position)
         div.setAttribute('class', 'snake')
+        snake.head = position;
+        snake.tail = [];
     }
 
     function createApple() {
@@ -52,32 +98,64 @@ window.addEventListener('load', function(e) {
         div.setAttribute('class', 'apple')
     }
 
+    function willCollide() {
+        return false
+    }
+
+    function willEat(newSnakeId) {
+        const apple = document.querySelector('.apple');
+        const appleId = apple.getAttribute("id");
+
+        if(newSnakeId == appleId) {
+            //eating
+            console.log("Eating..");
+            eatingSoundEl.onplay();
+            apple.classList.remove('apple');
+            createApple()
+            return true;
+        }
+        return false   
+    }
+
     function moveSnake() {
-        const snake = document.querySelector('.snake');
-        snake.classList.remove("snake");
-        snake.classList.add("pixel");
-        const oldSnakeId = parseInt(snake.getAttribute('id'));
+        const oldSnakeId = parseInt(snake.head)
         let newSnakeId;
 
         switch (snakeDirection){
-            case 'RIGHT':
+            case RIGHT:
                 newSnakeId = oldSnakeId + 1;
                 break;
-            case 'LEFT':
-                newSnakeId = oldSnakeId -1;
+            case LEFT:
+                newSnakeId = oldSnakeId - 1;
                 break;
-            case 'UP':
+            case UP:
                 newSnakeId = oldSnakeId - 200;
                 break;
-            case 'DOWN':
+            case DOWN:
                 newSnakeId = oldSnakeId + 200;
                 break;
             default:
                 newSnakeId = oldSnakeId + 1;
         }
-        const newSnake = document.getElementById(newSnakeId);
-    
-        newSnake.setAttribute('class','snake')
+        const newSnakeEl = document.getElementById(newSnakeId);
+
+        /** DETECT COLLISION */
+        if(willCollide()){
+            alert("Game over")
+        }
+        else {
+            //newSnakeEl.setAttribute('class','.snake');
+            newSnakeEl.classList.add('snake');
+            snake.tail.unshift(snake.head);
+            snake.head = newSnakeId;
+
+            if(!willEat(newSnakeId) ) {
+                const removedId = snake.tail.pop();
+                const removedEl = document.getElementById(removedId);
+                removedEl.classList.remove('snake');
+                removedEl.classList.add('pixel')
+            }
+        }  
     } 
 
    createBoard()
@@ -89,10 +167,10 @@ window.addEventListener('load', function(e) {
         if(GAME_LOOP){
             //ascolta eventi tastiera
             //move snake
-            moveSnake()
+            moveSnake();
             //check for collision
-            console.log("print snake every 500ms ")
+            console.log("move snake every " + fast + " ms ")
         }
-   }, 250) 
+   }, fast) 
   
 })
